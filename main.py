90c4f22b6b9b307d10ca24582eeb34ea63600c18
Elvis <91163415+elvis0411@users.noreply.github.com>
2021-12-27 21:51:11 -0800

    turtule example

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Screen, Turtle
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("dark olive gree")
+
+screen = Screen()
+screen.exitonclick()
+
